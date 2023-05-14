cf9fdbbaed69fb0585485146e0c1d750179a3a19
tanguy.cavagna <tanguy.cavagna@etu.hesge.ch>
2023-05-14 09:20:16 +0200

    add: wallpaper

--- arch.sh
+++ arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="i3,i3status,nvim,zsh,neofetch,alacritty,rofi,tmux"
+    STOW_FOLDERS="i3,i3status,nvim,zsh,neofetch,alacritty,rofi,tmux,wallpapers"
 fi
 
 if [[ -z $DOTFILES ]]; then
