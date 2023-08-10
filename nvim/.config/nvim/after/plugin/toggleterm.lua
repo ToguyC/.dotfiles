432b22c4489c52f4b6d70d61871d37b21ba05582
tanguy.cavagna <tanguy.cavagna@etu.hesge.ch>
2023-08-10 08:58:16 +0200

    update: full config rewrite for lighter and faster experience

--- nvim/.config/nvim/after/plugin/toggleterm.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm direction=float<cr>")
-vim.keymap.set("t", "<C-h>", "<cmd>ToggleTerm<cr>")
+vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>")
+vim.keymap.set("t", "<C-x>", "<cmd>ToggleTerm<cr>")
--- nvim/.config/nvim/lua/toguy/packer.lua
+++ nvim/.config/nvim/lua/toguy/packer.lua
@@ -52,4 +52,6 @@ return require('packer').startup(function(use)
     use {"akinsho/toggleterm.nvim", tag = '*', config = function()
         require("toggleterm").setup()
     end}
+
+    use('mhartington/formatter.nvim')
 end)
